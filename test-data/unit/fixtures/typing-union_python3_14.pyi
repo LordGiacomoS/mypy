e4b3cd06c393286b3d822b11ac4d5abcd15f6bba
L_GS <jscyclops@gmail.com>
2026-07-30 08:46:02 -0400

    set up tests to ensure unions are consistent and derive from `typing.Union`

---
+++ test-data/unit/fixtures/typing-union_python3_14.pyi
@@ -0,0 +1,17 @@
+# Test stub for typing module that includes 3.14 Union related things.
+#
+# Use [typing fixtures/typing-union_python3_14.pyi] to use this instead of lib-stub/typing.pyi
+# in a particular test case.
+#
+# Many of the definitions have special handling in the type checker, so they
+# can just be initialized to anything.
+
+assert_type = 0
+Generic = 0
+TypeVar = 0
+
+class Iterable: pass
+class Mapping: pass
+class Sequence: pass
+
+class Union: pass
