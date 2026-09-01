c3efc7807c1a77445a482a9ab137276d4018a5ea
James Simonetti <james.a.simonetti@gmail.com>
2026-09-01 14:31:53 -0400

    fix instance equivalence

--- test-data/unit/fixtures/union_python3_14.pyi
+++ test-data/unit/fixtures/union_python3_14.pyi
@@ -1,15 +1,16 @@
 # builtins stub used in test cases related to 3.14's union changes.
 
-from typing import Generic, Sequence, TypeVar
+from typing import Generic,  Sequence, TypeVar
 from types import UnionType
 T = TypeVar("T")
+S = TypeVar("S")
 
 class object:
     def __init__(self) -> None: pass
 class type:
     def __call__(self, *a: object) -> object: pass
-    def __or__(self, other: T) -> UnionType | T: pass
-    def __ror__(self, other: T) -> UnionType | T: pass
+    def __or__(self: S, other: T) -> S | T: pass
+    def __ror__(self: S, other: T) -> S | T: pass
 
 class ellipsis: pass
 
